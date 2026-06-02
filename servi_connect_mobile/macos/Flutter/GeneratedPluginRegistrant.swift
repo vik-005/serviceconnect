f00a794a75e836d4e851d593acc90f8fc700a572
vikence <providencekoukoui.com>
2026-06-02 20:41:12 +0200

    build: update flutter generated plugin configurations

--- servi_connect_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ servi_connect_mobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -14,7 +14,6 @@ import flutter_local_notifications
 import flutter_secure_storage_macos
 import geolocator_apple
 import just_audio
-import path_provider_foundation
 import record_macos
 import sqflite_darwin
 import url_launcher_macos
@@ -30,7 +29,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   RecordMacOsPlugin.register(with: registry.registrar(forPlugin: "RecordMacOsPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
